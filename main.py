fa8bef9e2062d33219ed1ad63679cd6e460d6205
sujeetgh <sujeetmehta6@gmail.com>
2022-03-21 17:33:56 +0530

    some progress

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
-from dotenv import load_dotenv
-from flask import Flask, render_template
-
-load_dotenv()
-
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return render_template("index.html")
-
-@app.route("/packages")
-def packages():
-    return render_template("packages.html")
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi.requests import Request
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static/"), "static")
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/")
+async def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.get("/packages")
+async def packages(request: Request):
+    return templates.TemplateResponse("packages.html", {"request": request})
+
+
+@app.get("/position")
+async def position(request: Request):
+    return templates.TemplateResponse("position/index.html", {"request": request})
